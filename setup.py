76b62c0e0300cdba47bba2ded903a75899b18e2d
Surya 'Zi <worksuryakencana@gmail.com>
2017-03-15 13:54:05 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'baka_test',
+  packages = ['baka_test'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Nanang Suryadi',
+  author_email = 'nanang.jobs@gmail.com',
+  url = 'https://github.com/suryakencana/baka_test', # use the URL to the github repo
+  download_url = 'https://github.com/suryakencana/baka_test/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
